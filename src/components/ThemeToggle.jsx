cbb5f588ca3bd99e8e48feaaca203071be3247d2
Audipunk <audipunkwp@gmail.com>
2025-03-29 01:52:54 +0200

    App.css

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { MoonIcon, SunIcon } from '@radix-ui/react-icons';
+import { ButtonIcon,} from '@radix-ui/react-icons';
 
 function ThemeToggle({ theme, toggleTheme, size = 18 }) {
    return (
      <button onClick={toggleTheme} className="theme-toggle">
-       {theme === 'light' ? <MoonIcon width={size} height={size} /> : <SunIcon width={size} height={size} />}
+       {theme === 'light' ? <button width={size} height={size} /> : <ButtonIcon width={size} height={size} />}
      </button>
    );
  }
